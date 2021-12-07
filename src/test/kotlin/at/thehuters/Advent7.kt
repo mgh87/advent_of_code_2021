408aca7e139e081bb9a569cc7f7aa4add7ee3464
Martin Huter <martin.huter@viesure.io>
2021-12-07 12:48:28 +0100

    Day 7: 1+2 Star refactor

--- src/test/kotlin/at/thehuters/Advent7.kt
+++ src/test/kotlin/at/thehuters/Advent7.kt
@@ -2,7 +2,6 @@ package at.thehuters
 
 import org.junit.jupiter.api.Test
 import kotlin.math.absoluteValue
-import kotlin.math.roundToInt
 
 
 class Advent7 {
@@ -11,32 +10,35 @@ class Advent7 {
         const val DAY="6"
     }
 
+    val identity = { n: Int -> n }
+    val sum = { n: Int -> n*(n+1)/2 }
+
     @Test
     fun advent_1_small() {
         val linesOfFile = readInput(DAY,"1")
-        advent6(linesOfFile)
+        advent6(linesOfFile, identity)
     }
 
     @Test
     fun advent_1_large() {
         val linesOfFile = readInput(DAY,"2")
-        advent6(linesOfFile)
+        advent6(linesOfFile, identity)
     }
 
     @Test
     fun advent_2_small() {
         val linesOfFile = readInput(DAY,"1")
-        advent6(linesOfFile)
+        advent6(linesOfFile, sum)
     }
 
     @Test
     fun advent_2_large() {
         val linesOfFile = readInput(DAY,"2")
-        advent6(linesOfFile)
+        advent6(linesOfFile, sum)
     }
 
 
-    private fun advent6(linesOfFile: List<String>) {
+    private fun advent6(linesOfFile: List<String>, fuelCost: (Int) -> Int) {
         val startFish = linesOfFile[0]
             .split(",")
             .map { s -> s.toInt()}
@@ -44,7 +46,7 @@ class Advent7 {
             .distinct()
             .map { position -> Pair(position, startFish.fold(0){ acc, i ->
                 val n = (i-position).absoluteValue
-                acc + n*(n+1)/2
+                acc + fuelCost(n)
             }) }
             .minByOrNull { pair ->
             pair.second
