7ba01e74183be10a33774f68dd66e6036f416066
Martin Huter <martin.huter@viesure.io>
2021-12-07 15:01:31 +0100

    Optimizing

--- src/test/kotlin/at/thehuters/Advent7.kt
+++ src/test/kotlin/at/thehuters/Advent7.kt
@@ -3,6 +3,7 @@ package at.thehuters
 import org.junit.jupiter.api.Test
 import kotlin.math.absoluteValue
 
+typealias Range = ( List<Int> ) -> IntRange
 
 class Advent7 {
 
@@ -11,40 +12,46 @@ class Advent7 {
     }
 
     private val identity = { n: Int -> n }
+    private val range1 : Range = { crabs: List<Int>  ->
+        val min = crabs.minOrNull()!!
+        val max = crabs.maxOrNull()!!
+        IntRange(min,max)
+    }
     private val sum = { n: Int -> n*(n+1)/2 }
+    private val range2 : Range = { crabs: List<Int>  ->
+        val avgFloor = crabs.average().toInt()
+        IntRange(avgFloor,avgFloor+1)
+    }
 
     @Test
     fun advent_1_small() {
         val linesOfFile = readInput(DAY,"1")
-        advent7(linesOfFile, identity)
+        advent7(linesOfFile, identity, range1)
     }
 
     @Test
     fun advent_1_large() {
         val linesOfFile = readInput(DAY,"2")
-        advent7(linesOfFile, identity)
+        advent7(linesOfFile, identity, range1)
     }
 
     @Test
     fun advent_2_small() {
         val linesOfFile = readInput(DAY,"1")
-        advent7(linesOfFile, sum)
+        advent7(linesOfFile, sum, range2)
     }
 
     @Test
     fun advent_2_large() {
         val linesOfFile = readInput(DAY,"2")
-        advent7(linesOfFile, sum)
+        advent7(linesOfFile, sum, range2)
     }
 
-
-    private fun advent7(linesOfFile: List<String>, fuelCost: (Int) -> Int) {
+    private fun advent7(linesOfFile: List<String>, fuelCost: (Int) -> Int, range: Range) {
         val splitCrabs = linesOfFile[0]
             .split(",")
             .map { s -> s.toInt()}
-        val min = splitCrabs.minOrNull()!!
-        val max = splitCrabs.maxOrNull()!!
-        IntRange(min,max).map { position -> Pair(position, splitCrabs.fold(0){ acc, i ->
+        range(splitCrabs).map { position -> Pair(position, splitCrabs.fold(0){ acc, i ->
             val n = (i-position).absoluteValue
             acc + fuelCost(n)
         }) }
@@ -52,7 +59,7 @@ class Advent7 {
                 pair.second
             }?.let {
                 println("$it")
-            }
+        }
     }
 
 }
